31d81e4e0b156d26e16915cc18350efefc1c021b
rix1 <rikardeide@gmail.com>
2014-08-15 05:38:14 +0200

    Fooled around trying to do all sorts of things, but with no luck. Did at least get a hold of the IP address (and other network interface attributes) Reported in https://app.asana.com/0/15290258811323/15351195406129

--- Phishlight/src/main/java/org/rix1/phishlight/MainActivity.java
+++ Phishlight/src/main/java/org/rix1/phishlight/MainActivity.java
@@ -3,18 +3,22 @@ package org.rix1.phishlight;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.hardware.Camera;
-import android.media.Image;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class MainActivity extends Activity {
 
@@ -24,6 +28,7 @@ public class MainActivity extends Activity {
     private boolean isFlashlightOn = false;
     private Camera camera;
     private Context context = this;
+    private PhishingHelper pHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +44,76 @@ public class MainActivity extends Activity {
         camera = Camera.open();
         final Camera.Parameters p = camera.getParameters();
 
-       // Added comment to test ASANA App
 
         toggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("APP: ", "Button clicked");
-                if(!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
-                    Toast.makeText(context, "No camera found ", Toast.LENGTH_LONG);
+
+                callHome();
+
+
+                if (!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+                    Toast.makeText(context, "No camera found ", Toast.LENGTH_LONG).show();
                 }
-                    if(isFlashlightOn) {
-                        Log.d("APP: ", "Flashlight is off");
-                        p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                        camera.setParameters(p);
-                        toggle.setText("ON");
-                        light.setVisibility(View.INVISIBLE);
-                        isFlashlightOn = false;
-                    }else{
-                        Log.d("APP: ", "Flashlight is ON!");
-                        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                        camera.setParameters(p);
-                        toggle.setText("OFF");
-                        light.setVisibility(View.VISIBLE);
-                        isFlashlightOn = true;
+                if (isFlashlightOn) {
+                    Log.d("APP: ", "Flashlight is off");
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(p);
+                    toggle.setText("ON");
+                    light.setVisibility(View.INVISIBLE);
+                    isFlashlightOn = false;
+                } else {
+                    Log.d("APP: ", "Flashlight is ON!");
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(p);
+                    toggle.setText("OFF");
+                    light.setVisibility(View.VISIBLE);
+                    isFlashlightOn = true;
                 }
             }
         });
+    }
 
+    public void callHome(){
+        String data = "Hi, heres my address: " + getAddress();
+        Toast.makeText(context, data, Toast.LENGTH_LONG);
+
+        /* This code is currently not working...
+        PhishingHelper phishingHelper = new PhishingHelper(context);
+        phishingHelper.insert(getAddress());
+        String extract = phishingHelper.getIP(phishingHelper.getAll());
+        Toast.makeText(context, extract, Toast.LENGTH_LONG).show();
+        */
+    }
+
+
+    public String getAddress(){
+            try {
+                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                    NetworkInterface intf = en.nextElement();
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            String ip = Formatter.formatIpAddress(inetAddress.hashCode());
+                            Log.d("APP: ", "***** IP="+ ip);
+                            return ip;
+                        }
+                    }
+                }
+            } catch (SocketException ex) {
+                Log.d("APP: ", ex.toString());
+            }
+            return null;
+        }
+
+    public void databaseTest(){
+        pHelper = new PhishingHelper(this);
+        String insert = "129.04.329294";
+        pHelper.insert(insert);
+        Cursor c = pHelper.getAll();
+        String extract = pHelper.getIP(c);
+        Toast.makeText(this, extract, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -95,3 +143,4 @@ public class MainActivity extends Activity {
         return true;
     }
 }
+	
--- Phishlight/src/main/java/org/rix1/phishlight/PhishingHelper.java
+++ Phishlight/src/main/java/org/rix1/phishlight/PhishingHelper.java
@@ -1,12 +1,14 @@
 package org.rix1.phishlight;
 
-import android.content.Context; 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteOpenHelper; 
 import android.database.sqlite.SQLiteDatabase; 
 import android.database.sqlite.SQLiteQueryBuilder;
 
-public class PhishingHelper {
+public class PhishingHelper extends SQLiteOpenHelper{
 	 private static final String DATABASE_NAME = "phishing.db";
 	 private static final int SCHEMA_VERSION = 1;
 	 
@@ -17,9 +19,14 @@ public class PhishingHelper {
 	  public void onCreate(SQLiteDatabase db) {
 		  db.execSQL("CREATE TABLE ip_address (_id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT);");  
 	  }
-	  
-	  public Cursor getAll() { 
-		  return(getReadableDatabase().rawQuery("SELECT _id, ip_address ORDER BY ip", null));  
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+
+    public Cursor getAll() {
+		  return(getReadableDatabase().rawQuery("SELECT _id, ip FROM ip_address ORDER BY ip", null));
 	  }
 	  
 	  //Put IP address into a ContentValues and tell SQLiteDatabase to insert
@@ -29,12 +36,11 @@ public class PhishingHelper {
 		  
 		  contV.put("ip", ip);
 		  
-		  getWritableDatabase().insert(ip, contV);
+		  getWritableDatabase().insert("ip_address", "ip", contV);
 	  }
 	  
 	  //Get IP address out of the cursor
 	  public String getIP(Cursor c){
-		  return(c.getIP());
+		  return(c.getString(1));
 	  }
-	  
 }
